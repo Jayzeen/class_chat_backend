d73356f4873bdae614491f7084b4f1f99128f9bd
Jayzeen <janithjayasinghe.nipuna@gmail.com>
2023-07-25 18:06:06 +0530

    Before connecting database

--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Machine Learning for noobs'},
+    {'id': 2, 'name': 'Designing for Pros'},
+    {'id': 3, 'name': 'Fullstack developers'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
