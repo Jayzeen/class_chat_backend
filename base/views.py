2f29a841d8030375a0af1c3319c1e8ebf4ccf3a0
Jayzeen <janithjayasinghe.nipuna@gmail.com>
2023-07-26 18:14:03 +0530

    Added DB models. Displayed queried data from db

--- base/views.py
+++ base/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
+from .models import Room, Message
 
 # Create your views here.
-
-rooms = [
-    {'id': 1, 'name': 'Machine Learning for noobs'},
-    {'id': 2, 'name': 'Designing for Pros'},
-    {'id': 3, 'name': 'Fullstack developers'},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            
-    context = {'room': room}
+    room = Room.objects.get(id=pk)
+    messages = Message.objects.filter(room=room)
+    context = {'room': room, 'messages': messages}
     return render(request, 'base/room.html', context)
